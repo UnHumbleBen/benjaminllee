6172842fcd1cfc2dcc0f240d19a27c193f6dc27e
Benjamin Lee <bnllee@ucdavis.edu>
2021-03-19 23:50:44 -0700

    Add navbar

--- index.js
+++ index.js
@@ -1,9 +1,54 @@
-var jumbotron = document.getElementById('jumbotron');
+const topElement = document.getElementById('top');
+const nav = document.querySelector('#navigation-bar');
 
+// Size the top element so that the transparent overlay is properly sized.
 function handleResize() {
-  jumbotron.style.height = window.innerHeight + 'px';
+  topElement.style.height = window.innerHeight + 'px';
 }
 
-window.addEventListener('resize', (_) => { handleResize() });
+// Fix issue with Scrollspy which causes wrong navigation item to be highlighted
+// because scroll offsets are miscalculated when page is loaded at non-zero
+// scroll position (i.e. loading a section in the middle of the page).
+// https://github.com/twbs/bootstrap/issues/32496#issuecomment-761728519
+function hotfixScrollSpy() {
+  curScroll = getCurrentScroll();
+  offsets = bootstrap.ScrollSpy.getInstance(document.body)['_offsets']
+  for (let i = 0; i < offsets.length; i++) {
+    offsets[i] += curScroll;
+  }
+}
 
-handleResize();
+function getCurrentScroll() {
+  return window.pageYOffset || document.documentElement.scrollTop;
+}
+
+// Changes navbar color and fill to white when scroll is at the top.
+function handleWhiteNavBar() {
+  if(this.scrollY < window.innerHeight) {
+    nav.classList.add('nav-white');
+  } else {
+    nav.classList.remove('nav-white');
+  }
+}
+
+// DEBUG: display offsets of sections
+function debugScrollSpy() {
+  console.log(bootstrap.ScrollSpy.getInstance(document.body)['_offsets'])
+}
+
+function main() {
+  handleResize();
+  window.addEventListener('resize', (_) => { handleResize() });
+  window.addEventListener('load', (_) => {
+    handleWhiteNavBar();
+    // For some reason, need to call this in order to make a ScrollSpy instance.
+    new bootstrap.ScrollSpy(document.body, {
+      target: '#navbarSupportedContent'
+    });
+    hotfixScrollSpy();
+    window.scrollBy(0,1);
+  });
+  window.addEventListener('scroll', () => { handleWhiteNavBar(); });
+}
+
+main();
